c2287947f2c7e9b0eca6fd7bca5a81b1b7e1aa84
aluno <aluno@ifpe>
2022-09-29 09:11:47 -0300

    enviando arquivos

---
+++ src/main/java/br/com/ifpe/oxxifoodVinicius/OxxifoodViniciusApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxxifoodVinicius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxxifoodViniciusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxxifoodViniciusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/oxxifoodVinicius/OxxifoodViniciusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxxifoodVinicius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxxifoodViniciusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
